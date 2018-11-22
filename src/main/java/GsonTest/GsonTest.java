a5d4caeb77e15cb2269868056d12e51326760078
Administrator <1341479360@qq.com>
2018-11-22 15:36:03 +0800

    修改3

--- src/main/java/GsonTest/GsonTest.java
+++ src/main/java/GsonTest/GsonTest.java
@@ -16,13 +16,7 @@ public class GsonTest {
 
     public static void main(String[] args) {
 
-
-        System.out.println("请输入");
-        System.out.println("修改1");
-        System.out.println("修改2");
-
-
-
+        System.out.println("111111");
 
     }
 
